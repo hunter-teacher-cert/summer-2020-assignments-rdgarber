6caa281cf87b7a8fd8f0c8ace2510eb58385d79d
Robert Garber <robdgarber@gmail.com>
2020-07-06 17:49:26 -0400

    Updated and tested code challenge

--- Classwork/SuperArray.java
+++ Classwork/SuperArray.java
@@ -8,10 +8,7 @@ public class SuperArray {
 	
 	public SuperArray()	{	// constructor
 	
-		data = new int [MAX_ELEMENTS];
-		numberElements = 0;
-		
-		
+		this(MAX_ELEMENTS);
 		
 	} // end SuperArray constructor
 	
@@ -20,24 +17,66 @@ public class SuperArray {
 		data = new int [size];
 		numberElements = 0;
 				
-	} // end SuperArray constructor
+	} // end SuperArray constructor with one argument
+	
+	// add one value to the array
+	//  index - location in array to add (e.g. insert)
+	//  value - element to add
+	 public void add(int index, int value)	{ 
+	/*  Old code before we added the grow() method:
+		if (numberElements  data.length)
+			data[numberElements++] = value;  // only add if there is room
+	*/
+			if (index > numberElements)
+				return; // do nothing here - this is not an insert - index is too big
+			
+			if (numberElements >= data.length) // We are at capacity (should never be greater than, but just in case....
+					grow();
+					
+			if (index == -1)	{   // add only
+				data[numberElements] = value; 
+			}
+			else {  // we are inserting
+				// lets move everything up past the insert point
+				for (int i=numberElements; i>index; i--)
+					data[i] = data[i-1];
+				// now insert the value
+				data[index] = value;
+			}
+			// now up the count
+			numberElements++;
+	
+		
+	} // end add
+
 	
 	// add one value to the array
 	 public void add(int value)	{ 
+	 
+		add(-1, value);
+	 
+	 } // end add
 	
-		if (numberElements < data.length)
-			data[numberElements++] = value;  // only add if there is room
+	// remove one from the array at position 'index'
+	public void remove(int index)	{
+		if (isEmpty() || get(index) == -1)
+			return; 	// Nothing there or index is too big
+		// nothing to move if we are removing the last element
+		for (int i = index; i< numberElements -1; i++)  
+			data[i] = data[i+1];
 		
-	
-	} // end add
+		numberElements--;
+		
+	} // end remove
 
+	
 	// get specific element from the array
 	public int get(int index)	{ 
 	
 		if (index >= numberElements)
 			return -1;
 		
-		return(data[numberElements]);
+		return(data[index]);
 		
 	
 	} // end get
@@ -56,6 +95,7 @@ public class SuperArray {
 	// array in a printable form
 	public String toString() {
 		
+		
 		String printableString = "";
 		
 		for (int i=0; i< numberElements; i++){
@@ -67,5 +107,29 @@ public class SuperArray {
 		return printableString;
 		
 	} // end toString
+	
+	
+	/*  This should increase the capacity of your SuperArray:
+		Make a new array with extra room.
+		Copy the data from the original internal array over to the new one.
+		Set the internal instance variable data to the new array.
+	*/
+	public void grow()		{
 		
+		int newdata[];
+		
+				
+		newdata = new int [data.length + MAX_ELEMENTS];
+		// now copy everything over
+		for (int i=0; i< numberElements; i++){
+			
+			newdata[i] = data[i];
+			
+		}
+		
+		data = newdata;		// reset the pointer to the new data
+		
+	} // end grow
+	
+	
 } // end class SuperArray
