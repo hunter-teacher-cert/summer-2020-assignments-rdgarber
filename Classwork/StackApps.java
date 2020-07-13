2e3fbe4e95880d5380c73ad11bbf9736e3aa4297
Robert Garber <robdgarber@gmail.com>
2020-07-13 12:50:50 -0400

    First two functions workinng

---
+++ Classwork/StackApps.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+  
+ // Stack Application
+ public class StackApps {
+ 
+	Stack<Character> theString = new Stack<String>();
+ 
+ 
+ 
+ 
+ 
+ }  // end StackApps
