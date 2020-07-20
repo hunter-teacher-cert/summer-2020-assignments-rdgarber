940d36d679b64dfa19ad8defa0f0e47b6ff29f54
Robert Garber <robdgarber@gmail.com>
2020-07-20 18:46:39 -0400

    Implemented Insert, did it a little different so we stay in the main loop to insert

--- Classwork/Day 14/BSTree.java
+++ Classwork/Day 14/BSTree.java
@@ -15,7 +15,6 @@ public class BSTree {
 		
 		while (current != null)		{
 			int currentValue = current.getData();
-			System.out.println(currentValue);
 			if (currentValue == key)	// we found it!
 				return currentValue;
 			else if (currentValue < key)
@@ -28,7 +27,39 @@ public class BSTree {
 			
 	} // end search
 	
+		// Search the tree for a specific key
+	public void insert(int key) {
+		TreeNode t;
+		TreeNode current = root;
+		TreeNode parent;		// keep track of the parent
+		
+		t = new TreeNode(key);	// create our new node;
+		if (current == null) { // there is no root
+			// make this the root;
+			root = t;
+			return;		// we are done
+		}
+		
+		while (current != null)		{  // traverse the tree until we get a null
+			parent = current;	// keep track of this when we find a null
+			int currentValue = current.getData();
+			if (currentValue == key)	// we found it!
+				throw new IllegalArgumentException("Node with key " + key + " already exists");
+			else if (currentValue < key)  {
+				current = current.getRight();	// key is higher, must go right
+				if (current == null) // this is where the key should have been if it had existed
+					parent.setRight(t);	// add the new node to the tree here
+			}
+			else    {// key is lower than current, go left
+				current = current.getLeft();
+				if (current == null) // this is where the key should have been if it had existed
+					parent.setLeft(t);	// add the new node to the tree here
+			}
+		}
 	
+			
+	} // end search
+	 
 
     public void seed(){
 	TreeNode t;
