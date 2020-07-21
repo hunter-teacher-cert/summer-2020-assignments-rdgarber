e1229d444b404a69de8616a917fdc12b257b9b19
Robert Garber <robdgarber@gmail.com>
2020-07-21 13:51:39 -0400

    Cleaned up Insert and put in shell for Delete routine

--- Classwork/Day 14/BSTree.java
+++ Classwork/Day 14/BSTree.java
@@ -27,39 +27,156 @@ public class BSTree {
 			
 	} // end search
 	
-		// Search the tree for a specific key
+		// Insert a new node into the tree
 	public void insert(int key) {
-		TreeNode t;
-		TreeNode current = root;
+		TreeNode newNode;
+		TreeNode front = root;
 		TreeNode parent;		// keep track of the parent
 		
-		t = new TreeNode(key);	// create our new node;
-		if (current == null) { // there is no root
+		newNode = new TreeNode(key);	// create our new node;
+		if (front == null) { // there is no root
 			// make this the root;
-			root = t;
+			root = newNode;
 			return;		// we are done
 		}
 		
-		while (current != null)		{  // traverse the tree until we get a null
-			parent = current;	// keep track of this when we find a null
-			int currentValue = current.getData();
-			if (currentValue == key)	// we found it!
+		while (front != null)		{  // traverse the tree until we get a null
+			parent = front;	// keep track of this when we find a null
+			int frontValue = front.getData();
+			if (frontValue == key)	// we found it!
 				throw new IllegalArgumentException("Node with key " + key + " already exists");
-			else if (currentValue < key)  {
-				current = current.getRight();	// key is higher, must go right
-				if (current == null) // this is where the key should have been if it had existed
-					parent.setRight(t);	// add the new node to the tree here
+			else if (frontValue < key)  {
+				front = front.getRight();	// key is higher, must go right
+				if (front == null) // this is where the key should have been if it had existed
+					parent.setRight(newNode);	// add the new node to the tree here
 			}
-			else    {// key is lower than current, go left
-				current = current.getLeft();
-				if (current == null) // this is where the key should have been if it had existed
-					parent.setLeft(t);	// add the new node to the tree here
+			else    {// key is lower than front, go left
+				front = front.getLeft();
+				if (front == null) // this is where the key should have been if it had existed
+					parent.setLeft(newNode);	// add the new node to the tree here
 			}
 		}
 	
 			
-	} // end search
+	} // end insert
+	
+	// Delete a node from the tree
+	public void delete(int key) {
+		TreeNode front = root;
+		TreeNode parent;		// keep track of the parent
+		
+		
+		if (front == null) { // there is no root
+			return;		// we are done
+		}
+		
+		// find the node that we want to delete
+		// keep track of the node above it
+		
+		while (front != null && front.getData() != key)		{  // traverse the tree until we get a null
+			parent = front;	// keep track of this when we find a null
+			int frontValue = front.getData();
+			if (frontValue < key)  {
+				front = front.getRight();	// key is higher, must go right
+			}
+			else    {// key is lower than front, go left
+				front = front.getLeft();
+			}
+		}
+		// if the key is not in the tree
+		if (front == null)
+			return;		// the key isn't there
+		
+		// if we get here front points to the node we want to delete
+		// case 1 - the node we want to delete is a leaf
+		if (front.getLeft() == null && front.getRight() == null)	{
+			// repoint front's parent to null
+			
+		}	
+		// case 2 check if front has one child
+		else if (true)  {
+			
+			
+			
+		}
+		// case 3 front has two children
+		// find the node with the largest value on front's left subtree
+		// and replace front with it.
+		else {
+			
+			
+		}	
+		
+			
+	} // end delete
+	
+	// recursively traverse 
+	private void preorderTraverse(TreeNode current) {
+		 if (current ==null)
+			 return;
+		 
+		// process the current nodeS
+		System.out.print(current.getData() + ", ");
+		
+		// recursively print out the left subnode
+		preorderTraverse(current.getLeft());
+		
+		// recursively print out the right subnode
+		preorderTraverse(current.getRight());
+	} // end private preorderTraverse
 	 
+	 public void preorderTraverse()	{
+		 preorderTraverse(root); 
+		 System.out.println();
+	 
+	 } // end public preorderTraverse
+	 
+	 	// recursively postorderTraverse 
+	private void postorderTraverse(TreeNode current) {
+		 if (current ==null)
+			 return;
+		 
+		
+		// recursively print out the left subnode
+		postorderTraverse(current.getLeft());
+		
+		// recursively print out the right subnode
+		postorderTraverse(current.getRight());
+		
+		// process the current nodeS
+		System.out.print(current.getData() + ", ");
+		
+	} // end private postorderTraverse2
+	 
+	 public void postorderTraverse()	{
+		 postorderTraverse(root);
+		 System.out.println();
+	 } // end public postorderTraverse
+	 
+	 
+	// recursively postorderTraverse
+	
+	private void inorderTraverse(TreeNode current) {
+		 if (current ==null)
+			 return;
+		
+		// recursively print out the left subnode
+		inorderTraverse(current.getLeft());
+		 
+		// process the current nodeS
+		System.out.print(current.getData() + ", ");
+		
+		// recursively print out the right subnode
+		inorderTraverse(current.getRight());
+	} // end private inorderTraverse
+	 
+	 public void inorderTraverse()	{
+		 inorderTraverse(root);
+		  System.out.println();
+
+	 
+	 } // end public inorderTraverse
+
 
     public void seed(){
 	TreeNode t;
--- Classwork/Day 14/Driver.java
+++ Classwork/Day 14/Driver.java
@@ -6,6 +6,7 @@ public class Driver {
     public static void main(String[] args) {
 	BSTree t = new BSTree();
 	
+	/*
 	t.seed();
 	System.out.printf("Search for 8: %s\n",t.search(8));
 	System.out.printf("Search for 9: %s\n",t.search(9));
@@ -13,6 +14,34 @@ public class Driver {
 	System.out.printf("Search for 21: %s\n",t.search(21));
 	System.out.printf("Search for 20: %s\n",t.search(20));
 	
-	
+	int value;
+
+		value = t.search(10);
+		System.out.println(value);
+
+		value = t.search(15);
+		System.out.println(value);
+
+		try {
+			value = t.search(17);
+			System.out.println(value);
+		} catch (NullPointerException e) {
+			System.out.println("17 not in tree");
+		}
     }
-}
+	*/
+
+		t.insert(10);
+		t.insert(20);
+		t.insert(5);
+		t.insert(7);
+		t.insert(8);
+		t.insert(3);
+		t.insert(25);
+		
+		t.preorderTraverse();
+		t.postorderTraverse();
+		t.inorderTraverse();
+	
+	}
+}	
